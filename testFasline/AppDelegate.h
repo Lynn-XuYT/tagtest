7abd94362285a7bbdef0635b524ac7799f409f39
lynne <xuyoutian01@corp.netease.com>
2019-06-02 22:04:19 +0800

    feat: init proj

---
+++ testFasline/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testFasline
+//
+//  Created by Lynn on 2019/5/30.
+//  Copyright © 2019 Lynn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testFasline/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testFasline
+//
+//  Created by Lynn on 2019/5/30.
+//  Copyright © 2019 Lynn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
